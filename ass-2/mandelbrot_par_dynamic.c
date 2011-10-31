755c811844fa9edf1c843d4d65c4b9e2f59ff00e
Owen Healy <ellbur@gmail.com>
2011-10-30 21:12:30 -0400

    dynamic

--- ass-2/mandelbrot_par_dynamic.c
+++ ass-2/mandelbrot_par_dynamic.c
@@ -81,24 +81,18 @@ static void print_hash(int len, const char image_data[len]) {
 // Parallelization Code
 
 #define num_children 7
-
-static void calc_offsets(
-    int proc,
-    int *row_start,
-    int *row_stop,
-    int *buf_len
-)
-{
-    int i = proc - 1;
-    *row_start = (height+num_children-1)/num_children*i;
-    
-    if (i == num_children-1) *row_stop = height;
-    else *row_stop = (height+num_children-1)/num_children*(i+1);
-    
-    *buf_len = (*row_stop - *row_start)*width/8;
-}
+#define buf_len ((width+7)/8)
 
 static void collect_data(char *image_data);
+static void start_row(
+    MPI_Request *pending_requests,
+    int *pending_rows,
+    char *buf,
+    int row,
+    int child
+);
+static void collect_into_image(char *image_data, int row, char *buf);
+static void send_shutdown(int child);
 
 static void master_routine() {
     char image_data[width * height * 3];
@@ -109,26 +103,70 @@ static void master_routine() {
 }
 
 static void collect_data(char *image_data) {
-    int buf_max_len = (height+num_children-1)/num_children*width/8;
-    char buf[buf_max_len];
+    int buf_max_len = buf_len;
+    char bufs[num_children][buf_max_len];
     
-    for (int p=1; p<(num_children+1); p++) {
-        int row_start, row_stop, buf_len;
-        calc_offsets(p, &row_start, &row_stop, &buf_len);
+    MPI_Request pending_requests[num_children];
+    int pending_rows[num_children];
+    int sent_rows = 0;
+    int completed_rows = 0;
+    
+    for (int i=0; i<num_children; i++) {
+        start_row(pending_requests, pending_rows, bufs[i], i, i);
+        sent_rows++;
+    }
+    
+    while (completed_rows < height) {
+        int recvd;
+        MPI_Waitany(num_children, pending_requests, &recvd, MPI_STATUS_IGNORE);
         
-        MPI_Status status;
-        MPI_Recv(&buf, buf_len, MPI_CHAR, p, 0, MPI_COMM_WORLD, &status);
-        
-        for (int i=row_start; i<row_stop; i++)
-        for (int j=0; j<width; j++) {
-            int pix = i*width+j;
-            int pix_off = (i-row_start)*width+j;
-            char in = (buf[pix_off/8] >> (pix_off%8)) & 1;
-            
-            image_data[3*pix+0] = in-1;
-            image_data[3*pix+1] = in-1;
-            image_data[3*pix+2] = in-1;
+        if (recvd == MPI_UNDEFINED) {
+            fprintf(stderr, "For some reason we received no more messages...");
+            exit(1);
         }
+        collect_into_image(image_data, pending_rows[recvd], bufs[recvd]);
+        completed_rows++;
+        
+        if (sent_rows < height) {
+            start_row(pending_requests, pending_rows, bufs[recvd], sent_rows, recvd);
+            sent_rows++;
+        }
+    }
+    
+    for (int i=0; i<num_children; i++) {
+        send_shutdown(i);
+    }
+}
+
+static void start_row(
+    MPI_Request *pending_requests,
+    int *pending_rows,
+    char *buf,
+    int row,
+    int child
+)
+{
+    MPI_Send(&row, 1, MPI_INT, child+1, 0, MPI_COMM_WORLD);
+    
+    MPI_Irecv(buf, buf_len, MPI_CHAR, child+1, 0,
+        MPI_COMM_WORLD, pending_requests+child);
+    pending_rows[child] = row;
+}
+
+static void send_shutdown(int child) {
+    int msg = -1;
+    MPI_Send(&msg, 1, MPI_INT, child+1, 0, MPI_COMM_WORLD);
+}
+
+static void collect_into_image(char *image_data, int row, char *buf) {
+    for (int i=0; i<buf_len; i++)
+    for (int j=0; j<8; j++) {
+        int pix = row*width + i*8 + j;
+        char in = (buf[i] >> j) & 1;
+        
+        image_data[3*pix+0] = in-1;
+        image_data[3*pix+1] = in-1;
+        image_data[3*pix+2] = in-1;
     }
 }
 
@@ -138,18 +176,22 @@ static void child_routine(int proc) {
     min_y = center_y - delta/2;
     max_y = center_y + delta/2;
     
-    int row_start, row_stop, buf_len;
-    calc_offsets(proc, &row_start, &row_stop, &buf_len);
-    
-    char buf[buf_len];
-    for (int i=0; i<buf_len; i++) buf[i] = 0;
-    
-    for (int i=row_start; i<row_stop; i++)
-    for (int j=0; j<width; j++) {
-        int pix_off = (i-row_start)*width+j;
-        buf[pix_off/8] |= calc_in(j, i) << (pix_off % 8);
+    while (true) {
+        int row;
+        MPI_Status status;
+        
+        MPI_Recv(&row, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
+        if (row == -1) break;
+        
+        char buf[buf_len];
+        for (int j=0; j<buf_len; j++) buf[j] = 0;
+        for (int j=0; j<width; j++) {
+            buf[j/8] |= calc_in(j, row) << (j % 8);
+        }
+        
+        MPI_Send(buf, buf_len, MPI_CHAR, 0, 0, MPI_COMM_WORLD);
     }
-    MPI_Send(&buf, buf_len, MPI_CHAR, 0, 0, MPI_COMM_WORLD);
+    
     MPI_Finalize();
 }
 
@@ -167,4 +209,5 @@ int main(int argc, char **argv) {
     }
     else child_routine(proc);
 }
+
 
