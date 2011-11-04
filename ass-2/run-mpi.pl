0cc8139eb7c2d273fab2f3c8e5872868cb34ce63
Owen Healy <ellbur@gmail.com>
2011-11-04 06:48:14 -0400

    forgot-this

---
+++ ass-2/run-mpi.pl
@@ -0,0 +1,41 @@
+#!/usr/bin/env perl
+
+use Env qw(DRY);
+
+$target = 'mpi-data.txt';
+
+`echo > $target`;
+
+my @queue = ();
+
+sub run {
+    my ($cmd) = @_;
+    push(@queue, $cmd);
+}
+
+sub process {
+    my $N = $#queue+1;
+    my $n = 1;
+    
+    for $cmd (@queue) {
+        print "\033[34m[$n/$N]\033[0m \033[36m$cmd\n\033[0m";
+        if (!$DRY) {
+            `$cmd`;
+        }
+        $n++;
+    }
+}
+
+@mpiprogs = ('./mpi-static', './mpi-dyn', './mpi-static-shuffle',
+    './mpi-dyn-shuffle', './mpi-omp-static', './mpi-omp-dyn');
+
+for $prog (@mpiprogs) {
+    for $i (2 ... 14) {
+        run("mpirun -n $i $prog >> $target");
+    }
+}
+
+run("./mandelbrot-nodisp >> $target");
+
+process()
+
