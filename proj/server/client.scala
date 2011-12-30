a19493bb2f25d7fd584bba2a70256e9cbe2cc357
Owen Healy <ellbur@gmail.com>
2011-12-29 21:52:24 -0500

    stuff

--- proj/server/client.scala
+++ proj/server/client.scala
@@ -122,7 +122,7 @@ class MultiClient(level: Int) {
         private def pause() {
             val max = (100 - level)*4
             val pause =
-                if (max > 0) Rantdom nextInt max
+                if (max > 0) Random nextInt max
                 else 0
                 
             Thread sleep pause
